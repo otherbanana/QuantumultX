4d19b0b0ebb303284ed8f08b7f699573acfa6d40
otherbanana <34509569+otherbanana@users.noreply.github.com>
2023-12-01 17:23:15 +0800

    Add files via upload

---
+++ File-js/jdcookie.js
@@ -0,0 +1,23 @@
+/*
+
+京东抓取pt_key，自用
+
+[rewrite_local]
+^https:\/\/mars\.jd\.com\/log\/sdk\/v2 -url script-request-header jdcookie.js
+
+[mitm] 
+hostname = mars.jd.com
+
+**/
+
+var header = $request.headers;
+if (header.Cookie !== undefined) {
+  var cookies = header.Cookie;
+  var options = {
+    "update-pasteboard": cookies,
+    "open-url": "shortcuts://run-shortcut?name=获取京东cookie"
+  };
+  console.log(cookies);
+  $notify("京东最新pt_key", "获取成功🎉🎉🎉", "点击复制并跳转快捷指令", options);
+}
+$done();
