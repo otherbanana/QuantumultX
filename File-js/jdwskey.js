d0758e93cc61a39084c45af1e9b01f2f605a749a
otherbanana <34509569+otherbanana@users.noreply.github.com>
2024-06-25 22:12:11 +0800

    Update jdwskey.js

--- File-js/jdwskey.js
+++ File-js/jdwskey.js
@@ -3,19 +3,6 @@
 京东抓取wskey，自用
 
 [rewrite_local]
-^https:\/\/api\.m\.jd\.com\/client\.action\?functionId= url script-request-header https://raw.githubusercontent.com/otherbanana/QuantumultX/main/File-js/jdwskey.js
+^https:\/\/api\.m\.jd\.com\/client\.action\?functionId= url script-request-header jdwskey.js
 [mitm] 
 hostname = api.m.jd.com
-
-**/
-var header = $request.headers;
-if (header.Cookie !== undefined) {
-  var cookies = header.Cookie;
-  var options = {
-    "update-pasteboard": cookies,
-    "open-url": "shortcuts://run-shortcut?name=获取京东wskey"
-  };
-  console.log(cookies);
-  $notify("京东最新wskey", "获取成功🎉🎉🎉", "点击复制并跳转快捷指令", options);
-}
-$done();
